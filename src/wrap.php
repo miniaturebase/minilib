b9cc63e89a8d107b6ff1d243109f572c4dba2946
jordanbrauer <18744334+jordanbrauer@users.noreply.github.com>
2019-06-06 22:35:25 -0500

    add wrap function to wrap things in arrays if not already

---
+++ src/wrap.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Helpers;
+
+/**
+ * Wrap a given value in an array if it is not already one.
+ *
+ * @param mixed $subject The subject value to be wrapped with an array.
+ * @return array
+ */
+function wrap($subject): array {
+    if (is_null($subject)) return [];
+
+    return (is_array($subject)) ? $subject : [$subject];
+}
