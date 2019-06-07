ac6ae34d16df5d5b9004718d637ecf1b255f9a12
jordanbrauer <18744334+jordanbrauer@users.noreply.github.com>
2019-06-06 20:18:37 -0500

    add appender function

---
+++ src/append.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Helpers;
+
+/**
+ * Append an item to a given subject.
+ *
+ * @param array|string $tail
+ * @param array|string $subject
+ * @param string $delimiter
+ * @return array|string
+ */
+function append($tail, $subject, string $delimiter = '') {
+    if (empty($tail)) {
+        return $subject;
+    }
+    
+    if (\is_array($subject)) {
+        $subject[] = $tail;
+
+        return $subject;
+    }
+    
+    if (\is_string($tail) and (\is_string($subject) or \is_numeric($subject))) {
+        return $subject.$delimiter.$tail;
+    }
+}
