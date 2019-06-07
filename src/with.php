2e05c3c265b492a3aa2abf251c3273c331c9bd54
jordanbrauer <18744334+jordanbrauer@users.noreply.github.com>
2019-06-06 22:35:11 -0500

    add with function

---
+++ src/with.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Helpers;
+
+use Closure;
+
+/**
+ * Simply returns the given subject. If the optional closure is provided, the
+ * subject will be passed to it and the result of the closure will be returned.
+ *
+ * @param mixed $subject The value to return or pass to the optional closure.
+ * @param Closure $closure An optional closure that receives the subject to process.
+ * @return mixed
+ */
+function with($subject, Closure $closure = null) {
+    return ($closure) ? $closure($subject) : $subject;
+}
