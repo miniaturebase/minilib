a68ccd26ce2b50ae9c3a0301609630acdbf6055e
jordanbrauer <18744334+jordanbrauer@users.noreply.github.com>
2019-06-08 09:49:46 -0500

    add an only function to extract array items

---
+++ src/only.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Helpers;
+
+/**
+ * Return a new array containing only the specified key/keys.
+ *
+ * @param array $array The array to extract keys and their values from.
+ * @param string|array $keys A singular key or an array of keys to create a new array from the given array.
+ * @return array
+ */
+function only(array $array, $keys): array {
+    $new = [];
+
+    foreach (wrap($keys) as $key) $new[$key] = $array[$key];
+
+    return $new;
+}
