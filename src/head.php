bc55920c2e334b7daca96b7b8926c163560092ca
jordanbrauer <18744334+jordanbrauer@users.noreply.github.com>
2019-06-08 10:03:25 -0500

    add function to get first item (head) of an array

---
+++ src/head.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Helpers;
+
+use Traversable;
+
+/**
+ * Return the first item in an array.
+ *
+ * @param array $items An array of associative or indexed items.
+ * @return mixed
+ */
+function head(array $items) {
+    return (!count($items)) ? null : array_values($items)[0] ?? null;
+}
