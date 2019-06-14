c1f9263f66c4ed9d671275a4d73920f445ea4391
jordanbrauer <18744334+jordanbrauer@users.noreply.github.com>
2019-06-13 22:57:07 -0500

    replace for loop with goto

--- src/retry.php
+++ src/retry.php
@@ -11,19 +11,32 @@ use Throwable;
  * Retry the provided closure equal to times given, and wait for the interval
  * between each try. Any exceptions or errors thrown in each try will be
  * swallowed, except for exceptions caught on attempts greater than the times.
+ * 
+ * @see https://github.com/igorw/retry/issues/3
  *
  * @param integer $times The amount of times to execute the closure.
- * @param integer $interval The amount of time **in seconds** to wait after each failed try.
  * @param Closure $closure The closure to retry failed executions for.
+ * @param integer $interval The amount of time **in seconds** to wait after each failed try.
  * @return mixed
  */
-function retry(int $times, int $interval, Closure $closure) {
-    for ($i = 1; /* condition in catch */; $i++) {
-        try {
-            return with($i, $closure);
-        } catch (Throwable $e) {
-            if ($i >= $times) throw $e;
-            sleep($interval);
+function retry(int $times, Closure $closure, int $interval = 0) {
+    $times--;
+
+    beginning:
+    
+    try {
+        return $closure();
+    } catch (Throwable $exception) {
+        if (!$times) {
+            throw $exception;
         }
+
+        if ($interval) {
+            \sleep($interval);
+        }
+
+        $times--;
+        
+        goto beginning;
     }
 }
