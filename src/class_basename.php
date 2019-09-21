771022e0ba19b7d2c9644aeb9ebb0317e101c42e
jordanbrauer <18744334+jordanbrauer@users.noreply.github.com>
2019-09-21 14:50:12 -0500

    improve class_basename function

--- src/class_basename.php
+++ src/class_basename.php
@@ -4,13 +4,25 @@ declare(strict_types = 1);
 
 namespace Phelpers;
 
+use InvalidArgumentException;
+
 /**
  * Return the base name of a given FQCN string, without the namespace.
  *
- * @param string $class The FQCN string to process.
+ * @param string|object $class The FQCN string or instance of an object.
  * @param string $delimiter The delimiter that sits between each namespace segment.
  * @return string
  */
-function class_basename(string $class, string $delimiter = '\\'): string {
-    return tail(explode($delimiter, $class));
+function class_basename($class, string $delimiter = '\\'): string {
+    $isObject = \is_object($class);
+
+    if (!$isObject and !\is_string($class)) {
+        throw new InvalidArgumentException(\sprintf('Argument 1 passed to %s must be of the type %s, %s given', __FUNCTION__, 'string|object', \gettype($class)));
+    }
+
+    if (!$isObject and blank($class)) {
+        return '';
+    }
+    
+    return tail(explode($delimiter, ($isObject) ? \get_class($class) : $class));
 }
