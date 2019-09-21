9b7b0a4ecaa6fae370b8b17654439a7511dbfce9
Jordan Brauer <jordan.brauer@pepper.com>
2019-09-20 22:13:40 -0500

    add helper function to check if a number is in range

---
+++ src/between.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Phelpers;
+
+use InvalidArgumentException;
+
+/**
+ * Determine if a numeric value is within range
+ *
+ * @return bool
+ */
+function between($subject, $min, $max): bool {
+    if (!is_numeric($subject)) {
+        throw new InvalidArgumentException(\sprintf('Argument 1 passed to %s must be of the type numeric, %s given', __FUNCTION__, $type));
+    }
+    
+    return $subject >= $min and $subject <= $max;
+}
