bd8959bed6f372f087b4f73b6e8d03cfe54cd54a
jordanbrauer <18744334+jordanbrauer@users.noreply.github.com>
2019-09-21 14:05:55 -0500

    test all scalar, compound, special, and closure values

--- tests/ValueTest.php
+++ tests/ValueTest.php
@@ -4,13 +4,78 @@ declare(strict_types = 1);
 
 namespace Phelpers\Tests;
 
-use function Phelpers\value;
+use function Phelpers\{value, random_float, array_make};
 use PHPUnit\Framework\TestCase;
+use Iterator;
 
 final class ValueTest extends TestCase
 {
-    public function test(): void
+    /**
+     * @covers Phelpers\value
+     * @dataProvider scalarValues
+     * @dataProvider compoundValues
+     * @dataProvider specialValues
+     * @param mixed $value
+     * @return void
+     */
+    public function testVariable($value): void
     {
-        $this->assertSame('test', value('test'));
+        $actual = value($value);
+        
+        $this->assertEquals($value, $actual);
+        $this->assertSame($value, $actual);
+    }
+
+    /**
+     * @covers Phelpers\value
+     * @dataProvider scalarValues
+     * @dataProvider compoundValues
+     * @dataProvider specialValues
+     * @param mixed $value
+     * @return void
+     */
+    public function testClosure($value): void
+    {
+        $closure = function ($x) {
+            return $x;
+        };
+        $actual = value($closure($value));
+        
+        $this->assertEquals($value, $actual, 'Failed to test value equaility of resolving of closures');
+        $this->assertSame($value, $actual, 'Failed to test identity equaility of resolving of closures');
+    }
+
+    public function scalarValues(): Iterator
+    {
+        yield from [
+            'boolean' => [(bool) rand(0, 1)],
+            'integer' => [rand(PHP_INT_MIN, PHP_INT_MAX)],
+            'float' => [random_float(0, 1)],
+            'string' => ['jah-ith-ber'],
+        ];
+    }
+
+    public function compoundValues(): Iterator
+    {
+        yield from [
+            'array' => [
+                array_make(3),
+            ],
+            'object' => [
+                (object) [
+                    'runes' => ['tal', 'thul', 'ort', 'amn'],
+                    'name' => 'Spirit',
+                    'reqlvl' => 54,
+                ]
+            ]
+        ];
+    }
+
+    public function specialValues(): Iterator
+    {
+        yield from [
+            'null' => [null],
+            'resource' => [fopen(__FILE__, 'r')],
+        ];
     }
 }
