613bf5d60b98fe7118602370794f0099b83586fd
jordanbrauer <18744334+jordanbrauer@users.noreply.github.com>
2019-09-21 16:23:10 -0500

    add partial functional helper

---
+++ src/partial.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Phelpers;
+
+use Closure;
+
+/**
+ * Return a partial closure from the given callable and arguemnts. The closure
+ * will accept all remaining arguments of the original callable.
+ *
+ * @param callable $function The callable to create a partial from
+ * @param mixed ...$arguments A list of arguments to supply to the callable
+ * @return Closure
+ */
+function partial(callable $function, ...$arguments): Closure {
+    return function (...$nextArguments) use ($function, $arguments) {
+        return $function(...array_merge($arguments, $nextArguments));
+    };
+}
