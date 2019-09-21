1f6706f55cba60af620455f490a976954c492044
jordanbrauer <18744334+jordanbrauer@users.noreply.github.com>
2019-09-21 00:08:54 -0500

    improve append function

--- src/append.php
+++ src/append.php
@@ -4,26 +4,28 @@ declare(strict_types = 1);
 
 namespace Phelpers;
 
+use InvalidArgumentException;
+
 /**
  * Append an item to a given subject.
  *
- * @param array|string $tail
+ * @param mixed $tail
  * @param array|string $subject
  * @param string $delimiter
  * @return array|string
  */
 function append($tail, $subject, string $delimiter = '') {
-    if (empty($tail)) {
-        return $subject;
+    if (\is_string($subject) and !(\is_string($tail) or \is_numeric($tail))) {
+        throw new InvalidArgumentException(\sprintf('Arguments 1 and 2 passed to %s must be of the type %s, %s|%s given', __FUNCTION__, 'string', \gettype($tail), \gettype($subject)));
+    } else if (!\is_array($subject) and !\is_string($subject)) {
+        throw new InvalidArgumentException(\sprintf('Argument 2 passed to %s must be of the type %s, %s given', __FUNCTION__, 'array', \gettype($subject)));
     }
-    
+
     if (\is_array($subject)) {
         $subject[] = $tail;
 
         return $subject;
     }
-    
-    if (\is_string($tail) and (\is_string($subject) or \is_numeric($subject))) {
-        return $subject.$delimiter.$tail;
-    }
+
+    return $subject.$delimiter.$tail;
 }
