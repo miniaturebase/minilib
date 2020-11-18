89e2d272dddc88ad715a55104ae774175d3d5d81
jordanbrauer <18744334+jordanbrauer@users.noreply.github.com>
2020-11-18 17:42:13 -0600

    add exists function

---
+++ src/exists.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Phelpers;
+
+/**
+ * Attempt to determine if the given array or object contains the given path (a
+ * dot sperated path of keys/properties).
+ *
+ * @param array|object $subject The subject to look for the existence of a key on
+ * @param string $path A key/property or dot seperated path of keys/properties to check for existence
+ * @return bool
+ */
+function exists($subject, string $path): bool {
+    $noop = \md5(\sprintf('%s.%s.%d', str_random(), $path, \rand()));
+
+    return read($subject, $path, $noop) !== $noop;
+}
