d2403e1321de125994f3f45e6ba23e18b2e84200
jordanbrauer <18744334+jordanbrauer@users.noreply.github.com>
2021-02-09 18:41:21 -0600

    more house keeping

--- src/bits.php
+++ src/bits.php
@@ -5,7 +5,7 @@ declare(strict_types = 1);
 /**
  * This file is part of the jordanbrauer/phelpers PHP library.
  *
- * @copyright 2020 Jordan Brauer <18744334+jordanbrauer@users.noreply.github.com>
+ * @copyright 2021 Jordan Brauer <18744334+jordanbrauer@users.noreply.github.com>
  * @license MIT
  *
  * For the full copyright and license information, please view the LICENSE
