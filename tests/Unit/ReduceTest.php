23172cb278a967d5ad8240610af92d8a33c4663d
jordanbrauer <18744334+jordanbrauer@users.noreply.github.com>
2021-05-14 23:59:47 -0500

    style: address linting & formatting issues

--- tests/Unit/ReduceTest.php
+++ tests/Unit/ReduceTest.php
@@ -2,6 +2,16 @@
 
 declare(strict_types = 1);
 
+/**
+ * This file is part of the minibase-app/minilib PHP library.
+ *
+ * @copyright 2021 Jordan Brauer <18744334+jordanbrauer@users.noreply.github.com>
+ * @license MIT
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use function Minibase\array_make;
 use function Minibase\reduce;
 use function Minibase\str_random;
